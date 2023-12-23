4716ac0cc728907da3a87bd40880451e1486f084
LukeMinnich <squareman2357@gmail.com>
2023-12-22 17:54:42 -0800

    Day 05 Part 2 WIP

--- 05/src/main.zig
+++ 05/src/main.zig
@@ -1,7 +1,13 @@
 const std = @import("std");
 const print = std.debug.print;
 const IdList = std.ArrayList(u32);
+const IdRangeList = std.ArrayList(IdRange);
 const MapRuleList = std.ArrayList(MapRule);
+
+const IdRange = struct {
+    start: u64,
+    count: u32,
+};
 
 const MapRule = struct {
     src_idx: u64,
@@ -11,7 +17,7 @@ const MapRule = struct {
 
 const MapIterator = std.mem.TokenIterator(u8, .scalar);
 
-fn parse_seeds(line: []const u8, seeds: *IdList) !void {
+fn parse_seeds_part1(line: []const u8, seeds: *IdList) !void {
     var splits = std.mem.splitAny(u8, line, ": ");
     _ = splits.next().?; // `seeds`
 
@@ -21,6 +27,32 @@ fn parse_seeds(line: []const u8, seeds: *IdList) !void {
         const seed_txt = std.mem.trim(u8, split, " ");
         const seed_num = try std.fmt.parseUnsigned(u32, seed_txt, 10);
         try seeds.append(seed_num);
+    }
+}
+
+fn parse_seeds_part2(line: []const u8, seeds: *IdRangeList) !void {
+    var splits = std.mem.splitAny(u8, line, ": ");
+    _ = splits.next().?; // `seeds`
+
+    while (splits.next()) |split_left| {
+        if (split_left.len < 1)
+            continue;
+
+        // const split_right: []u8 = splits.next().?;
+
+        // zig fmt: off
+        const start_txt = std.mem.trim(u8, split_left,  " ");
+        const count_txt = std.mem.trim(u8, splits.next().?, " ");
+        // zig fmt: on
+
+        const start = try std.fmt.parseUnsigned(u64, start_txt, 10);
+        const count = try std.fmt.parseUnsigned(u32, count_txt, 10);
+        const range = IdRange{
+            .start = start,
+            .count = count,
+        };
+        // print("range: {}\n", .{range});
+        try seeds.append(range);
     }
 }
 
@@ -54,7 +86,21 @@ fn parse_map_rules(iter: *MapIterator, map_rules: *std.ArrayList(MapRuleList)) !
     }
 }
 
-fn map_seeds_by_rule_list(ids: IdList, rules: MapRuleList) void {
+fn map_seeds_by_rule_list_part1(ids: IdList, rules: MapRuleList) void {
+    id_loop: for (ids.items) |*id| {
+        for (rules.items) |rule| {
+            if (id.* >= rule.src_idx and id.* < rule.src_idx + rule.count) {
+                const src: i64 = @intCast(rule.src_idx);
+                const dst: i64 = @intCast(rule.dst_idx);
+                const sid: i64 = @intCast(id.*);
+                id.* = @intCast(sid + (dst - src));
+                continue :id_loop;
+            }
+        }
+    }
+}
+
+fn map_seeds_by_rule_list_part2(ids: IdList, rules: MapRuleList) void {
     id_loop: for (ids.items) |*id| {
         for (rules.items) |rule| {
             if (id.* >= rule.src_idx and id.* < rule.src_idx + rule.count) {
@@ -72,22 +118,28 @@ fn solve_puzzle(input: []const u8) !usize {
     var iter = std.mem.tokenizeScalar(u8, input, '\n');
 
     const seed_txt = iter.next().?;
-    var ids = IdList.init(arena.allocator());
-    parse_seeds(seed_txt, &ids) catch unreachable;
+    // var ids = IdList.init(arena.allocator());
+    // parse_seeds_part1(seed_txt, &ids) catch unreachable;
+
+    var id_ranges = IdRangeList.init(arena.allocator());
+    parse_seeds_part2(seed_txt, &id_ranges) catch unreachable;
 
     _ = iter.next().?; // blank line
 
     var all_map_rules = std.ArrayList(MapRuleList).init(arena.allocator());
     try parse_map_rules(&iter, &all_map_rules);
 
+    // for (all_map_rules.items) |rule_list|
+    //     map_seeds_by_rule_list_part1(ids, rule_list);
+
     for (all_map_rules.items) |rule_list|
-        map_seeds_by_rule_list(ids, rule_list);
+        map_seeds_by_rule_list_part2(id_ranges, rule_list);
 
     var lowest: u32 = std.math.maxInt(u32);
-    for (ids.items) |id| {
-        if (id < lowest)
-            lowest = id;
-    }
+    // for (ids.items) |id| {
+    //     if (id < lowest)
+    //         lowest = id;
+    // }
 
     return lowest;
 }
